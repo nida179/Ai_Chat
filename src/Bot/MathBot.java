8661e56426f3394318ddef21a7efcb1ff807b95b
nida179 <nidahafeez569@gmail.com>
2025-07-20 08:28:10 -0700

    Update MathBot.java

--- src/Bot/MathBot.java
+++ src/Bot/MathBot.java
@@ -1,4 +1,26 @@
 package Bot;
-
-public class MathBot {
+import Interface.Bot_interface;
+public class MathBot implements Bot_interface{
+  
+    public void respond(String input){
+        switch(input){
+            case "add":
+                System.out.println("MathBot: Let's add the numbers ");
+                break;
+            case "sub":
+                System.out.println("MathBot: Let's subtract the numbers ");
+                break;
+            case "mul":
+                System.out.println("MathBot: Let's multiply the numbers ");
+                break;
+            case "div":
+                System.out.println("MathBot: Let's divide the numbers ");
+                break;
+            case "mod":
+                System.out.println("MathBot: Let's take the modulo of the numbers ");
+                break;
+            default:
+                System.out.println("I ONLY DO THE MATH'S STUFF");
+        }
+    }
 }
